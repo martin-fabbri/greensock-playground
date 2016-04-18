8d5d04dc0adc5dd7fb2536ce50f18f86d3dcb064
Martin Fabbri <martin.fabbri.a@gmail.com>
2016-04-17 23:05:58 -0700

    TweenMax p2

--- greenshock-101/14-tweenmax/main.js
+++ greenshock-101/14-tweenmax/main.js
@@ -2,15 +2,18 @@
 /// <reference path="../../typings/main/ambient/TweenMax/TweenMax.d.ts" />
 /// <reference path="../../typings/main/ambient/TweenLite/TweenLite.d.ts" />
 /// <reference path="../../typings/main/ambient/TimelineLite/TimelineLite.d.ts" />
+/// <reference path="../../typings/main/ambient/jquery/index.d.ts" />
 require('TweenLite');
 require('CSSPlugin');
+require('AttrPlugin');
 var TimelineLite = require('TimelineLite');
+var $ = require('jquery');
 //region JavaScript Element Selectors
 var box = document.getElementsByClassName('box');
 var boxSVG = document.getElementById('boxSVG');
 var boxHTML = document.getElementById('boxHTML');
 var htmlDot = document.getElementById('htmlDOT');
-var svgDot = document.getElementById('svgcircle');
+var svgDot = $('#boxSVG .circ');
 //endregion
 //region Main Timeline
 var tl = new TimelineLite({ paused: true });
@@ -26,7 +29,25 @@ tl
     .addPause()
     .to(boxSVG, 0.7, { left: '50%', top: '50%', x: '-50%', y: '-50%', ease: Power2.easeInOut }) //50% of the whole viewport size
     .to(svgDot, 0.7, { attr: { cx: 0, cy: 0 }, ease: Power2.easeInOut })
-    .addPause();
+    .addPause()
+    .to(boxHTML, 0.7, { rotation: 90, transformOrigin: '100% 100%', ease: Power2.easeInOut })
+    .addPause()
+    .to(boxSVG, 0.7, { rotation: 90, transformOrigin: '100% 100%', ease: Power2.easeInOut })
+    .addPause()
+    .to(boxHTML, 0.7, { xPercent: -100, rotation: 0, ease: Power2.easeInOut })
+    .addPause()
+    .to(boxSVG, 0.7, { x: '0%', rotation: 0, ease: Power2.easeInOut })
+    .addPause()
+    .to(htmlDot, 0.7, { left: '50%', top: '50%', ease: Power2.easeInOut })
+    .to(svgDot, 0.7, { attr: { cx: 100, cy: 100 }, ease: Power2.easeInOut }, '-=0.7')
+    .addPause()
+    .to([boxHTML, boxSVG], 0.7, { rotation: 720, transformOrigin: '50% 50%', ease: Power2.easeInOut })
+    .addPause()
+    .to(boxHTML, 0.7, { rotationX: -180, transformOrigin: '0% 50%', ease: Power2.easeInOut })
+    .to(boxSVG, 0.7, { rotationY: -180, transformOrigin: '100% 50%', ease: Power2.easeInOut })
+    .addPause()
+    .to(boxHTML, 0.7, { rotationX: -360, ease: Power2.easeInOut })
+    .to(boxSVG, 0.7, { rotationY: -360, ease: Power2.easeInOut });
 //endregion
 //region Botton Next
 var btnNext = document.getElementById('btnNext');
@@ -34,9 +55,6 @@ var btnPrev = document.getElementById('btnPrev');
 //endregion
 //region Botton Next
 btnNext.onclick = function () {
-    console.log('Play Next');
-    console.log(boxHTML);
-    console.log(tl);
     tl.play();
 };
 btnPrev.onclick = function () {
--- greenshock-101/14-tweenmax/main.ts
+++ greenshock-101/14-tweenmax/main.ts
@@ -1,17 +1,20 @@
 /// <reference path="../../typings/main/ambient/TweenMax/TweenMax.d.ts" />
 /// <reference path="../../typings/main/ambient/TweenLite/TweenLite.d.ts" />
 /// <reference path="../../typings/main/ambient/TimelineLite/TimelineLite.d.ts" />
+/// <reference path="../../typings/main/ambient/jquery/index.d.ts" />
 import 'TweenLite';
 import 'CSSPlugin';
+import 'AttrPlugin';
 import * as TweenMax from 'TweenMax';
 import * as TimelineLite from 'TimelineLite';
+import * as $ from 'jquery';
 
 //region JavaScript Element Selectors
 const box = document.getElementsByClassName('box');
 const boxSVG = document.getElementById('boxSVG');
 const boxHTML = document.getElementById('boxHTML');
 const htmlDot = document.getElementById('htmlDOT');
-const svgDot = document.getElementById('svgcircle');
+const svgDot = $('#boxSVG .circ');
 //endregion
 
 //region Main Timeline
@@ -27,8 +30,26 @@ tl
     .to(htmlDot, 0.7, {left: '0', top: '0', ease:Power2.easeInOut})
     .addPause()
     .to(boxSVG, 0.7, {left: '50%', top: '50%', x: '-50%', y: '-50%', ease:Power2.easeInOut})    //50% of the whole viewport size
-    .to(svgDot, 0.7, {attr: {cx: 0, cy:0}, ease:Power2.easeInOut})
+    .to(svgDot, 0.7, {attr:{cx: 0, cy: 0}, ease:Power2.easeInOut})
     .addPause()
+    .to(boxHTML, 0.7, {rotation: 90, transformOrigin: '100% 100%', ease:Power2.easeInOut})
+    .addPause()
+    .to(boxSVG, 0.7, {rotation: 90, transformOrigin: '100% 100%', ease:Power2.easeInOut})
+    .addPause()
+    .to(boxHTML, 0.7, {xPercent: -100, rotation: 0, ease:Power2.easeInOut})
+    .addPause()
+    .to(boxSVG, 0.7, {x: '0%', rotation: 0, ease:Power2.easeInOut})
+    .addPause()
+    .to(htmlDot, 0.7, {left: '50%', top: '50%', ease:Power2.easeInOut})
+    .to(svgDot, 0.7, {attr:{cx: 100, cy: 100}, ease:Power2.easeInOut}, '-=0.7')
+    .addPause()
+    .to([boxHTML, boxSVG], 0.7, {rotation: 720, transformOrigin: '50% 50%', ease:Power2.easeInOut})
+    .addPause()
+    .to(boxHTML, 0.7, {rotationX: -180, transformOrigin: '0% 50%', ease:Power2.easeInOut})
+    .to(boxSVG, 0.7, {rotationY: -180, transformOrigin: '100% 50%', ease:Power2.easeInOut})
+    .addPause()
+    .to(boxHTML, 0.7, {rotationX: -360, ease:Power2.easeInOut})
+    .to(boxSVG, 0.7, {rotationY: -360, ease:Power2.easeInOut})
 ;
 
 //endregion
@@ -40,9 +61,6 @@ const btnPrev = document.getElementById('btnPrev');
 
 //region Botton Next
 btnNext.onclick = ()=> {
-    console.log('Play Next');
-    console.log(boxHTML);
-    console.log(tl);
     tl.play();
 }
 
